de4da374d54e658f339859dcfb6393e0e749cc58
nombre <xx@yy.com>
2024-02-15 17:48:52 -0500

    Modularization test

---
+++ main.py
@@ -0,0 +1,25 @@
+import modules
+
+def main(pregunta):
+    pages = docLoader("./pdfs")
+    texto_final = txt_final(pages)
+    texts = textSplitterConfig ( 750 , 75, texto_final)
+    
+    #modelos sugeridos
+    #'sentence-transformers/paraphrase-multilingual-MiniLM-L12-v2' # 471M
+    #'sentence-transformers/paraphrase-multilingual-mpnet-base-v2' #1.11G
+    embeddings = embeddingSetup ('sentence-transformers/paraphrase-multilingual-mpnet-base-v2')
+    knowledge_base = vectorStoreCreation (texts, embeddings)
+    llm, chain = openAIConfig(model_names="gpt-3.5-turbo", chains="stuff")
+
+    # Busqueda de párrafos similares
+    docs = knowledge_base.similarity_search(pregunta, 3)
+    # Utilizar los parrafos similares para darle contexto a ChatGPT
+    respuesta = chain.run(input_documents=docs, question=pregunta)
+    print(f"Respuesta ChatGPT: {respuesta}")
+
+
+
+
+if __name__ == "__main__":
+    main()
--- modules.py
+++ modules.py
@@ -4,65 +4,90 @@ from langchain.embeddings import HuggingFaceEmbeddings
 from langchain.vectorstores import FAISS
 from langchain.chat_models import ChatOpenAI
 from langchain.chains.question_answering import load_qa_chain
+from langchain.vectorstores import FAISS
+from dotenv import load_dotenv
+import os
 
 
+load_dotenv()
 #DOCUMENT LOADER
-loader = PyPDFLoader("./pdfs/prueba3.pdf")
-pages = loader.load_and_split()
+
+def docLoader(directorio_pdf ):
+    # Directorio donde se encuentran los archivos PDF directorio_pdf 
+    # Lista para almacenar las páginas de todos los archivos PDF
+    pages = []
+
+    # Obtener la lista de archivos PDF en el directorio
+    archivos_pdf = os.listdir(directorio_pdf)
+
+    # Recorrer la lista de archivos PDF
+    for archivo_pdf in archivos_pdf:
+
+        # Cargar el archivo PDF
+        loader = PyPDFLoader(os.path.join(directorio_pdf, archivo_pdf))
+
+        # Dividir el archivo PDF en páginas
+        paginas_archivo = loader.load_and_split()
+
+        # Agregar las páginas del archivo actual a la lista global
+        pages.extend(paginas_archivo)
+
+    # Imprimir la cantidad de páginas cargadas
+    print("DOCUMENT LOADER cargado con Éxito.")
+    print(f"Se cargaron un total de {len(pages)} páginas")
+
+    # Retornar la lista de páginas
+    return pages
+
+def txt_final (documentsDocumentLoader):
+    #variable vacia para el output
+    texto_final = ""
+
+    for indice, Document in enumerate(pages):
+        # Se agrega el page_content de cada documento al final de la variable texto_final
+        texto_final += Document.page_content
+
+        # Si no es el último documento, se agrega un salto de página
+        if indice < len(pages) - 1:
+            texto_final += "\n\n"
+
+    #   Se imprime el contenido final de la variable texto_final
+    print("A continuación los primeros 500 caracteres del texto: " + "\n" + texto_final[:500])
+    return texto_final
+
+def textSplitterConfig (chunk_sz , chunk_ol, texto_final ):
+    #   Se configura el textsplitter y se generan las particiones
+    text_splitter = CharacterTextSplitter(
+        separator="\n\n",
+        chunk_size=chunk_sz,
+        chunk_overlap=chunk_ol,
+        #length_function=len,
+        #is_separator_regex=False,
+    )   
+
+    texts = text_splitter.split_text(texto_final)
+    print(" TEXT SPlITTER aplicado con éxito. Se han creado " +  str(len(texts)) + " documents en total.")
+    return texts
 
 
-#TEXTSPLITTER
-text_splitter = CharacterTextSplitter(
-    separator="\n\n",
-    chunk_size=750,
-    chunk_overlap=75,
-    #length_function=len,
-    #is_separator_regex=False,
-)
 
-#FROM DOCUMENTS TO TXT
-texto_final = ""
-
-for indice, Document in enumerate(pages):
-    # Se agrega el page_content de cada documento al final de la variable texto_final
-    texto_final += Document.page_content
-
-    # Si no es el último documento, se agrega un salto de página
-    if indice < len(pages) - 1:
-        texto_final += "\n\n"
-
-# Se imprime el contenido final de la variable texto_final
-print(texto_final)
-
-#SPLITTING OF TXT
-texts = text_splitter.split_text(texto_final)
-
-#modelos sugeridos
-'sentence-transformers/paraphrase-multilingual-MiniLM-L12-v2' # 471M
-'sentence-transformers/paraphrase-multilingual-mpnet-base-v2' #1.11G
-
-
-#EMBEDDING SETUP
-embeddings = HuggingFaceEmbeddings(model_name="sentence-transformers/paraphrase-multilingual-mpnet-base-v2")
+def embeddingSetup (model):
+    #EMBEDDING SETUP
+    embeddings = HuggingFaceEmbeddings(model_name=model)
+    print ("Inicialización de Embeddings")
+    return embeddings
+    
 
 #VECTORSTORE CREATION
-knowledge_base = FAISS.from_texts(texts, embeddings)
+def vectorStoreCreation(archivo, embedding):
+    knowledge_base = FAISS.from_texts(archivo, embedding)
+    print ("VECTORSTORE CREADA CON EXITO ")
+    return knowledge_base
 
-#PREGUNTA Y EXTRACCION MUESTRAS
-pregunta = "¿Cual es el lugar en el que se desarrolla la problematica del texto?"
-docs = knowledge_base.similarity_search(pregunta, 3)
-
-#AUTENTICACION OPENAI
-os.environ["OPENAI_API_KEY"] = ""
-
-llm = ChatOpenAI(model_name='gpt-3.5-turbo')
-
-chain = load_qa_chain(llm, chain_type="stuff")
-
-#PREGUNTA
-pregunta = "¿Que fue la operacion mariscal en la comuna 13?"
-# Busqueda de párrafos similares
-docs = knowledge_base.similarity_search(pregunta, 3)
-# Utilizar los parrafos similares para darle contexto a ChatGPT
-respuesta = chain.run(input_documents=docs, question=pregunta)
-print(f"Respuesta ChatGPT: {respuesta}")
+#API KEY SETUP langchain open ai chain. LLM selector
+def openAIConfig(model_names, chains):
+    #os.environ["OPENAI_API_KEY"] = "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"
+    llm = ChatOpenAI(model_name=model_names)
+    chain = load_qa_chain(llm, chain_type=chains)
+    print("Configuración OPENAI lograda con éxito.")
+    return llm, chain
